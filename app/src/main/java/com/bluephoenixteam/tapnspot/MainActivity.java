e67aa5df66e858a5276c9a68d0600593e91aa2b9
Arturo Gonzalez <arthurgonzalezp@gmail.com>
2016-06-15 15:32:01 -0500

    Added Splash Screen
    Added the Splash Screen and implemented Material design

---
+++ app/src/main/java/com/bluephoenixteam/tapnspot/MainActivity.java
@@ -0,0 +1,14 @@
+package com.bluephoenixteam.tapnspot;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/com/bluephoenixteam/tapnspot/SplashScreenActivity.java
@@ -0,0 +1,31 @@
+package com.bluephoenixteam.tapnspot;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+
+public class SplashScreenActivity extends Activity {
+    private int SPLASH_TIME_OUT = 2000;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                // Login check (condition: !userIsLogged)
+                Intent loginIntent;
+                if(false){
+                    loginIntent = new Intent(SplashScreenActivity.this,LoginActivity.class);
+                }else{
+                    loginIntent = new Intent(SplashScreenActivity.this,MainActivity.class);
+                }
+                startActivity(loginIntent);
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
+    }
+}
